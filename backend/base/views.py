35d5dc49eb261d3e0d2d079b488b4be484c787cd
daylan <daylanberry@gmail.com>
2021-09-05 10:47:20 -0700

    building db and controllers

--- backend/base/views.py
+++ backend/base/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+
+from .models import Product
 from .products import products
+from .serializers import ProductSerializer
 
 # Create your views here.
 
@@ -12,16 +15,15 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
-  print(request)
-  return Response(products)
+  products = Product.objects.all()
+  # many - are we doing multiple objects
+  serializer = ProductSerializer(products, many=True)
+  return Response(serializer.data)
 
 @api_view(['GET'])
 def getProduct(request, pk):
-  product = None
-  
-  for product in products:
-    if product['_id'] == pk:
-      product = product
-      break
+  product = Product.objects.get(_id=pk)
+  serializer = ProductSerializer(product, many=False)
 
-  return Response(product)
+  return Response(serializer.data)
+  
