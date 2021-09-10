6d2824ece045f0bd5976063c6c2748ac7935b4b0
daylan <daylanberry@gmail.com>
2021-09-10 08:22:22 -0700

    creating user serializers

--- backend/base/views.py
+++ backend/base/views.py
@@ -5,9 +5,27 @@ from rest_framework.decorators import api_view
 
 from .models import Product
 from .products import products
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 # Create your views here.
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+  def validate(self, attrs):
+        data = super().validate(attrs)
+
+        serializer = UserSerializerWithToken(self.user).data
+        
+        for k, v in serializer.items():
+          data[k] = v
+
+        return data
+ 
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -19,6 +37,17 @@ def getProducts(request):
   # many - are we doing multiple objects
   serializer = ProductSerializer(products, many=True)
   return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getUserProfile(request):
+  user = request.user
+  products = Product.objects.all()
+  # many - are we doing multiple objects
+  serializer = UserSerializer(user, many=False)
+  return Response(serializer.data)
+
+
 
 @api_view(['GET'])
 def getProduct(request, pk):
