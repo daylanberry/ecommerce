301c0274d42ff642ff13b08b7261f709ab0c0fb0
daylan <daylanberry@gmail.com>
2021-09-12 14:28:45 -0700

    creating frontend for user

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from django.contrib.auth.models import User
-from .models import Product
-from .products import products
-from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from base.serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -15,7 +12,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
 
-# Create your views here.
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
   def validate(self, attrs):
@@ -50,18 +46,6 @@ def registerUser(request):
   except:
     message = { 'detail': 'User with this email already exists'}
     return Response(message, status=status.HTTP_400_BAD_REQUEST)
-    
-
-@api_view(['GET'])
-def getRoutes(request):
-  return Response('hi')
-
-@api_view(['GET'])
-def getProducts(request):
-  products = Product.objects.all()
-  # many - are we doing multiple objects
-  serializer = ProductSerializer(products, many=True)
-  return Response(serializer.data)
 
 
 @api_view(['GET'])
@@ -79,13 +63,3 @@ def getUsers(request):
 
   serializer = UserSerializer(users, many=True)
   return Response(serializer.data)
-
-
-@api_view(['GET'])
-def getProduct(request, pk):
-  print(pk)
-  product = Product.objects.get(_id=pk)
-  serializer = ProductSerializer(product, many=False)
-
-  return Response(serializer.data)
-  
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,19 +7,26 @@ import {
   productDetailsReducers,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReducers } from "./reducers/userReducers";
 
 const reducer = combineReducers({
   productList: productListReducers,
   productDetails: productDetailsReducers,
   cart: cartReducer,
+  userLogin: userLoginReducers,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
 
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : {};
+
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 
 const middleware = [thunk];
